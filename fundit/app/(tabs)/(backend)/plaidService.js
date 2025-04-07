cee465a0835419418d30221e5b3fc6d3a2c5ba05
Riya Prasad <riyap4@uw.edu>
2025-04-06 18:03:51 -0700

    updated step for creating a new link token

--- fundit/app/(tabs)/(backend)/plaidService.js
+++ fundit/app/(tabs)/(backend)/plaidService.js
@@ -37,10 +37,16 @@ app.post('/api/create_link_token', async function (request, response) {
     // Get the client_user_id by searching for the current user
     // const user = await User.find(...);
     // const clientUserId = user.id;
+    const { firebaseUid } = request.body;
+
+    if (!firebaseUid) {
+      return res.status(400).json({ error: 'firebaseUid is required' });
+    }
+
     const linkTokenRequest = {
       user: {
         // This should correspond to a unique id for the current user.
-        client_user_id: "test-client-user-id", // clientUserId
+        client_user_id: firebaseUid,
       },
       client_name: 'Plaid Test App',
       products: ['auth'],
